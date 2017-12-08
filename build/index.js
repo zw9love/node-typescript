61fcbb9f66a2a3a94d40d15aa7c7b36fa155dbcb
zw9love <18514075699@163.com>
2017-12-08 17:58:31 +0800

    delete build

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * @author zengwei
- * @since 2017/11/15
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./server/index");
-new index_1.default();
